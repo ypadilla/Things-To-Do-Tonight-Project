e2f608e96c846fa89aa9b5ad46aa98381ef94c38
Yavi Padilla <yavipadilla@theengineersair.local.valenciacc.edu>
2016-08-16 19:16:58 -0400

    add js file and google places API

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+$(window).load(function(){
+        $('#myModal').modal('show');
+    });
+
+async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZKR3YD85re6NPj2gFlDftsWV6-GG1D0E&libraries=places&callback=initMap" 
+
+
+        var map;
+
+function initMap() {
+    var pyrmont = {
+        lat: 28.5241838,
+        lng: -81.46383709999999
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 17
+    });
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 1500,
+        type: ['food']
+    }, processResults);
+}
+
+function processResults(results, status, pagination) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+    } else {
+        createMarkers(results);
+        if (pagination.hasNextPage) {
+            var moreButton = document.getElementById('more');
+            moreButton.disabled = false;
+            moreButton.addEventListener('click', function() {
+                moreButton.disabled = true;
+                pagination.nextPage();
+            });
+        }
+    }
+}
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
+    for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+        });
+        placesList.innerHTML += '<h4>' + place.name + '</h4>';
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
